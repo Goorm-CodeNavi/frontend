06e13a6b25ee5d1f9f1f299cad5960043ce3bf30
Jihyun <hjh20030615@gmail.com>
2025-10-16 21:28:14 +0900

    문제은행 메인 페이지 일부 완료

--- src/pages/bank/Bank.jsx
+++ src/pages/bank/Bank.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import Search from '../../assets/img/ic_search.svg';
+import Toggle from '../../assets/img/ic_arrow_down.svg';
 
 const Bank = () => {
     return (
         <div className='Bank_wrap'>
-            문제은행 화면입니다
+            <aside>
+                <div className="category">카테고리</div>
+                <div className="algorithms">
+                    전체<br />수학<br />구현<br />다이나믹 프로그래밍
+                </div>
+            </aside>
+            <main>
+                <div className="main_header">
+                    <div className="title">문제 은행</div>
+                    <div className="search_bar">
+                        <input className='search' placeholder='검색어를 입력하세요' />
+                        <img src={Search} alt="Search" />
+                    </div>
+                </div>
+                <div className="main_body">
+                    <div className="level_toggle">
+                        <div className="level">Easy</div>
+                        <img src={Toggle} alt="Toggle" />
+                    </div>
+                    <div className="bank">
+                        <table className="problem_lists">
+                            <thead>
+                                <tr>
+                                    <th>문제 번호</th>
+                                    <th>문제 제목</th>
+                                    <th>카테고리</th>
+                                    <th>정보</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* TODO: 백엔드에서 문제 목록 받아오기 */}
+                                문제 리스트를 최대 10개까지 보여줍니다.
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="main_footer">
+                    {/* 페이지네이션 */}
+                    <div className="pagination">
+                        1 2 3 4 5
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
