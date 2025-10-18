d4383bbfdf9aae43a570accabab9051ed9e999d5
Jihyun <hjh20030615@gmail.com>
2025-10-19 00:09:34 +0900

    feat: 마이페이지 내 정보 수정 화면 완료

--- src/pages/mypage/sections/EditInfo.jsx
+++ src/pages/mypage/sections/EditInfo.jsx
@@ -18,38 +18,48 @@ const EditInfo = () => {
                         <input type="text" placeholder={old_id} className='id_input' />
                         <div className="dupl_btn">중복 확인</div>
                     </div>
-                    <div className="warning">이미 삭용 중인 아이디입니다.</div>
+                    <div className="warning">이미 사용 중인 아이디입니다.</div>
                 </div>
                 <div className="edit_pw">
                     <div className="title">비밀번호</div>
                     <div className="new_pw">
-                        <input type="text" placeholder='새로운 비밀번호를 입력해 주세요' className="pw_input" />
+                        <div className="pw_input">
+                            <input type="text" placeholder='새로운 비밀번호를 입력해 주세요' />
+                        </div>
                         <img src={Hide} alt="Hide" />
                     </div>
                     <div className="check_new_pw">
-                        <input type="text" placeholder='비밀번호를 다시 입력해 주세요' className="pw_input" />
+                        <div className="pw_input">
+                            <input type="text" placeholder='비밀번호를 다시 입력해 주세요' />
+                        </div>
                         <img src={Hide} alt="Hide" />
                     </div>
                     <div className="warning">비밀번호가 일치하지 않습니다.</div>
                 </div>
                 <div className="edit_email">
                     <div className="title">이메일</div>
-                    <input type="text" placeholder={old_email} className="email_input" />
+                    <div className="email_input">
+                        <input type="text" placeholder={old_email} />
+                    </div>
                 </div>
                 <div className="marketing">
                     <div className="title">마케팅 활용 및 수신 동의</div>
-                    <div className="agree">
-                        <input type="checkbox" className="agree_email" />
-                        <div className="text">이메일</div>
-                    </div>
-                    <div className="agree">
-                        <input type="checkbox" className="agree_email" />
-                        <div className="text">SMS</div>
+                    <div className="agrees">
+                        <div className="agree">
+                            <input type="checkbox" className="agree_email" />
+                            <div className="text">이메일</div>
+                        </div>
+                        <div className="agree">
+                            <input type="checkbox" className="agree_email" />
+                            <div className="text">SMS</div>
+                        </div>
                     </div>
                 </div>
                 <div className="link_notion">
                     <div className="title">노션 연동하기</div>
-                    <input type="text" placeholder={notion_email} className="link_email" />
+                    <div className="link_email">
+                        <input type="text" placeholder={notion_email} />
+                    </div>
                 </div>
                 <div className="divider"></div>
                 <div className="finish_btn">내 정보 수정하기</div>
