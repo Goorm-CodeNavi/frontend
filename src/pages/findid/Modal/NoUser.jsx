3b33120be464d286d127369d29efb86f2b3ec2ed
Jihyun <hjh20030615@gmail.com>
2025-10-23 04:40:10 +0900

    faet: 파일 생성 완료

---
+++ src/pages/findid/Modal/NoUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoUser = () => {
+    return (
+        <div className='NoUser_wrap'>
+            해당 유저 정보가 없을 때 뜨는 모달입니다.
+        </div>
+    )
+}
+
+export default NoUser
---
+++ src/pages/findid/Modal/UserId.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserId = () => {
+    return (
+        <div className='UserId_wrap'>
+            해당 유저 정보가 있을 때 뜨는 모달입니다.
+        </div>
+    )
+}
+
+export default UserId
