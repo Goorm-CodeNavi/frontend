127840260e16921962da54b1e0446aaa0f51d52b
Jihyun <hjh20030615@gmail.com>
2025-10-23 15:16:50 +0900

    feat: 아이디 찾기 화면 구현 완료

--- src/pages/findid/Modal/NoUser.jsx
+++ src/pages/findid/Modal/NoUser.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NoUser = () => {
+    const navigate = useNavigate();
+    
     return (
         <div className='NoUser_wrap'>
-            해당 유저 정보가 없을 때 뜨는 모달입니다.
+            <div className="text">입력하신 이메일로 등록한 아이디가 없습니다.</div>
+            <div className="subtext">회원가입 후 이용해주시기 바랍니다.</div>
+            <div className="goto_signup" onClick={() => navigate("/signup")}>회원가입 하러가기</div>
         </div>
     )
 }
